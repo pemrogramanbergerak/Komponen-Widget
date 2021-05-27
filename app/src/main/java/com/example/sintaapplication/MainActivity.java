9ad82e686d9938baba283676945d75b93dd94cd1
Sintaa <sintawahyuni09112000@gmail.com>
2021-05-27 06:02:51 -0700

    first commit

---
+++ app/src/main/java/com/example/sintaapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sintaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            }
+}
